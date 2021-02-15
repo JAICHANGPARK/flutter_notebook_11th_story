30d17c69ccc72a288c3d657dd21ce7fa5a57deac
Dreamwalker <aristojeff@gmail.com>
2021-02-15 20:16:26 +0900

    Update ep701_clubhouse_clone.dart

--- lib/ep701_clubhouse/ep701_clubhouse_clone.dart
+++ lib/ep701_clubhouse/ep701_clubhouse_clone.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_11th_story/ep701_clubhouse/ui/clubhouse_home_page.dart';
 
 class ClubhouseClone extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+          home: ClubhouseHomePage(),
     );
   }
 }
