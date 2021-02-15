ee814de4e433f13d2db1628dea3831118a8bddde
Dreamwalker <aristojeff@gmail.com>
2021-02-15 20:15:21 +0900

    Create clubhouse_home_page.dart

---
+++ lib/ep701_clubhouse/ui/clubhouse_home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ClubhouseHomePage extends StatefulWidget {
+  @override
+  _ClubhouseHomePageState createState() => _ClubhouseHomePageState();
+}
+
+class _ClubhouseHomePageState extends State<ClubhouseHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
