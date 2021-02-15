eda461272d0275ed78690199c3e03ec253707d40
Dreamwalker <aristojeff@gmail.com>
2021-02-15 20:16:23 +0900

    Update clubhouse_home_page.dart

--- lib/ep701_clubhouse/ui/clubhouse_home_page.dart
+++ lib/ep701_clubhouse/ui/clubhouse_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_11th_story/ep701_clubhouse/ui/clubhouse_style.dart';
 
 class ClubhouseHomePage extends StatefulWidget {
   @override
@@ -9,7 +10,7 @@ class _ClubhouseHomePageState extends State<ClubhouseHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: ClubhouseStyle.appBackgroundColor,
     );
   }
 }
