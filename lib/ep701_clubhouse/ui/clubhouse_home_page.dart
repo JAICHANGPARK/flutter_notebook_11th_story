145906950343fcde203975bc4212597ab806a416
Dreamwalker <aristojeff@gmail.com>
2021-02-18 20:21:15 +0900

    Update clubhouse_home_page.dart

--- lib/ep701_clubhouse/ui/clubhouse_home_page.dart
+++ lib/ep701_clubhouse/ui/clubhouse_home_page.dart
@@ -483,7 +483,7 @@ bool isToggle = false;
                             ),
                           ),
                           Spacer(),
-                          isToggle ? IconButton(
+                          !isToggle ? IconButton(
                             icon: Icon(Icons.apps),
                             onPressed: () {
                               _toggle();
