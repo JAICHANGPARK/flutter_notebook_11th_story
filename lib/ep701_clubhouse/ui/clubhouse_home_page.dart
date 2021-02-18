05375e68747484b1f838418e5247c19743212366
Dreamwalker <aristojeff@gmail.com>
2021-02-18 20:14:34 +0900

    Update clubhouse_home_page.dart

--- lib/ep701_clubhouse/ui/clubhouse_home_page.dart
+++ lib/ep701_clubhouse/ui/clubhouse_home_page.dart
@@ -115,7 +115,7 @@ class _ClubhouseHomePageState extends State<ClubhouseHomePage> {
                                             child: Container(
                                               decoration: BoxDecoration(
                                                   color: Colors.brown[400],
-                                                  borderRadius: BorderRadius.circular(8)),
+                                                  borderRadius: BorderRadius.circular(12)),
                                             ),
                                           ),
                                           Positioned(
