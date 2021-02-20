206c59cf57f251103935b13e8c7872df3aa87194
Dreamwalker <aristojeff@gmail.com>
2021-02-20 19:33:58 +0900

    Update notification_page.dart

--- lib/ep701_clubhouse/ui/notification_page.dart
+++ lib/ep701_clubhouse/ui/notification_page.dart
@@ -10,6 +10,8 @@ class NotificationPage extends StatefulWidget {
 class _NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+
+    );
   }
 }
