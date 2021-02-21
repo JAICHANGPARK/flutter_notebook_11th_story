96090e11a47644dc0d5650a39c66b11a4f9a758b
Dreamwalker <aristojeff@gmail.com>
2021-02-21 17:51:16 +0900

    Update profile_page.dart

--- lib/ep701_clubhouse/ui/profile_page.dart
+++ lib/ep701_clubhouse/ui/profile_page.dart
@@ -43,23 +43,29 @@ class _ProfilePageState extends State<ProfilePage> {
       ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.only(left: 16, right: 16, bottom: 24),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                height: 84,
-                width: 84,
-                decoration: BoxDecoration(
-                  color: Colors.blue[100],
-                  borderRadius: BorderRadius.circular(32)
+              GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                      context: context,
+                      isScrollControlled: true,
+                      backgroundColor: Colors.transparent,
+                      builder: (context) => Container(
+                            height: MediaQuery.of(context).size.height / 1.09,
+                            decoration: BoxDecoration(color: ClubhouseStyle.appBackgroundColor),
+                          ));
+                },
+                child: Container(
+                  height: 84,
+                  width: 84,
+                  decoration: BoxDecoration(color: Colors.blue[100], borderRadius: BorderRadius.circular(32)),
                 ),
               ),
               Row(
-                children: [
-                  Text("Dreamwalker"),
-                  Text("(Flutter)")
-                ],
+                children: [Text("Dreamwalker"), Text("(Flutter)")],
               ),
               Text("@iamdreamwalker"),
               Row(
@@ -72,112 +78,99 @@ class _ProfilePageState extends State<ProfilePage> {
               ),
               Flexible(
                   child: GestureDetector(
-                  onTap: (){
-                    showModalBottomSheet(
-                      backgroundColor: Colors.transparent,
-                        isScrollControlled: true,
-                        context: context, builder: (context){
-                      return Container(
-                        height: MediaQuery.of(context).size.height / 1.1,
-                        decoration: BoxDecoration(
-                          color: ClubhouseStyle.appBackgroundColor,
-                          borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(16),
-                            topLeft: Radius.circular(16),
-                          )
-                        ),
-                        child: Center(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text("Update your bio", style: GoogleFonts.montserrat(
-                                fontSize: 16
-                              ),),
-                              Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: Container(
-                                  height: 160,
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(8)
-                                  ),
-                                  padding: EdgeInsets.all(8),
-                                  child: TextField(
-                                    controller: bioTextEditingController,
-                                    maxLines: null,
-                                    keyboardType: TextInputType.multiline,
-                                    style: GoogleFonts.montserrat(),
-                                    decoration: InputDecoration(
-                                      border: InputBorder.none
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              GestureDetector(
-                                onTap: (){
-                                  Navigator.of(context).pop();
-                                },
-                                child: Container(
-                                  height: 42,
-                                  width: 180,
-                                  decoration: ShapeDecoration(
-                                    shape: StadiumBorder(),
-                                    color: Colors.blue
-                                  ),
-                                  child: Center(
-                                    child: Text("Done", style: GoogleFonts.montserrat(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 16
-                                    ),),
+                      onTap: () {
+                        showModalBottomSheet(
+                            backgroundColor: Colors.transparent,
+                            isScrollControlled: true,
+                            context: context,
+                            builder: (context) {
+                              return Container(
+                                height: MediaQuery.of(context).size.height / 1.1,
+                                decoration: BoxDecoration(
+                                    color: ClubhouseStyle.appBackgroundColor,
+                                    borderRadius: BorderRadius.only(
+                                      topRight: Radius.circular(16),
+                                      topLeft: Radius.circular(16),
+                                    )),
+                                child: Center(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        "Update your bio",
+                                        style: GoogleFonts.montserrat(fontSize: 16),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(16.0),
+                                        child: Container(
+                                          height: 160,
+                                          decoration: BoxDecoration(
+                                              color: Colors.white, borderRadius: BorderRadius.circular(8)),
+                                          padding: EdgeInsets.all(8),
+                                          child: TextField(
+                                            controller: bioTextEditingController,
+                                            maxLines: null,
+                                            keyboardType: TextInputType.multiline,
+                                            style: GoogleFonts.montserrat(),
+                                            decoration: InputDecoration(border: InputBorder.none),
+                                          ),
+                                        ),
+                                      ),
+                                      GestureDetector(
+                                        onTap: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Container(
+                                          height: 42,
+                                          width: 180,
+                                          decoration: ShapeDecoration(shape: StadiumBorder(), color: Colors.blue),
+                                          child: Center(
+                                            child: Text(
+                                              "Done",
+                                              style: GoogleFonts.montserrat(
+                                                  color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16),
+                                            ),
+                                          ),
+                                        ),
+                                      )
+                                    ],
                                   ),
                                 ),
-                              )
-                            ],
-                          ),
-                        ),
-                      );
-                    });
-                  },
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none
-                          ),
-                          controller: bioTextEditingController,
-                          maxLines: null,
-                          keyboardType: TextInputType.multiline,
-                          enabled: false,
-                        ),
-                        Row(
+                              );
+                            });
+                      },
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text("Add to Instagram"),
-                            Text("Add to Twitter"),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            CircleAvatar(
-                              radius: 16,
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                            bioTextEditingController.text.length > 0? TextField(
+                              decoration: InputDecoration(border: InputBorder.none),
+                              controller: bioTextEditingController,
+                              maxLines: null,
+                              keyboardType: TextInputType.multiline,
+                              enabled: false,
+                            ) : Text("Add your bio"),
+                            Row(
                               children: [
-                                Text("Joined Feb 1, 2021"),
-                                Text("Nominated by Unknown")
+                                Text("Add to Instagram"),
+                                Text("Add to Twitter"),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                CircleAvatar(
+                                  radius: 16,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [Text("Joined Feb 1, 2021"), Text("Nominated by Unknown")],
+                                )
                               ],
                             )
                           ],
-                        )
-                      ],
-                    ),
-                  ))),
-
-
-
+                        ),
+                      ))),
             ],
           ),
         ),
@@ -185,21 +178,3 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
