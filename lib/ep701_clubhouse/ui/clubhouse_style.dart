b1151997e2011a70ca80d33ec71687097b285f91
Dreamwalker <aristojeff@gmail.com>
2021-02-23 19:35:52 +0900

    Update clubhouse_style.dart

--- lib/ep701_clubhouse/ui/clubhouse_style.dart
+++ lib/ep701_clubhouse/ui/clubhouse_style.dart
@@ -1,7 +1,6 @@
-
 import 'package:flutter/material.dart';
 
-class ClubhouseStyle{
+class ClubhouseStyle {
   //243 240 230  f3 f0 e6
   static const Color appBackgroundColor = Color(0xfff3f0e6);
 }
