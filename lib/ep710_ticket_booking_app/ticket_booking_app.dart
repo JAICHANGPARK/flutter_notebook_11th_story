c68e66e21cf94131f2e6815a14063dfa28406e8d
Dreamwalker <aristojeff@gmail.com>
2021-02-24 20:21:45 +0900

    Update ticket_booking_app.dart

--- lib/ep710_ticket_booking_app/ticket_booking_app.dart
+++ lib/ep710_ticket_booking_app/ticket_booking_app.dart
@@ -5,7 +5,7 @@ class TicketBookingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: TicketBookingHomePage(),
     );
   }
 }
