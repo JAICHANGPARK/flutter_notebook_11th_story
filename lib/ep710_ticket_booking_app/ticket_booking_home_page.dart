a61809312c5d27fac836d7f57d24d2c758589881
Dreamwalker <aristojeff@gmail.com>
2021-02-24 20:23:22 +0900

    Create ticket_booking_home_page.dart

---
+++ lib/ep710_ticket_booking_app/ticket_booking_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TicketBookingHomePage extends StatefulWidget {
+  @override
+  _TicketBookingHomePageState createState() => _TicketBookingHomePageState();
+}
+
+class _TicketBookingHomePageState extends State<TicketBookingHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
