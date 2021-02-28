6fbf28d1ecad942486ceb550ffd98e614ee91f99
Dreamwalker <aristojeff@gmail.com>
2021-02-28 18:16:15 +0900

    Update food_bill_home_page.dart

--- lib/ep714_food_bill_app/ui/food_bill_home_page.dart
+++ lib/ep714_food_bill_app/ui/food_bill_home_page.dart
@@ -12,20 +12,29 @@ class _FoodBillHomePageState extends State<FoodBillHomePage> {
       body: SafeArea(
         child: Stack(
           children: [
+
+            Positioned(child: Container(
+              height: MediaQuery.of(context).size.height / 1.2,
+              color: Colors.grey,
+            )
+
+            ),
             Positioned(
               right: 8,
               top: 0,
-              child: Text("PROFILE", style: TextStyle(
-                fontWeight: FontWeight.bold
-              ),),
+              child: Text(
+                "PROFILE",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
             ),
-            Positioned(child: Container(
-              width: 48,
-              color: Colors.black,
-              height: MediaQuery.of(context).size.height / 1.4,
-            ),
-            left: 0,
-            top: 0,
+            Positioned(
+              child: Container(
+                width: 48,
+                color: Colors.black,
+                height: MediaQuery.of(context).size.height / 1.4,
+              ),
+              left: 0,
+              top: 0,
             ),
             Positioned(
               bottom: 16,
@@ -36,9 +45,7 @@ class _FoodBillHomePageState extends State<FoodBillHomePage> {
                   child: Card(
                     elevation: 8,
                     child: Row(
-                      children: [
-
-                      ],
+                      children: [],
                     ),
                   )),
             )
