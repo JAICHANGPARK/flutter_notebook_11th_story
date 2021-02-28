eb097aa4bcf774ca11f208ac8269db67736216c0
Dreamwalker <aristojeff@gmail.com>
2021-02-28 18:09:45 +0900

    Create food_bill_home_page.dart

---
+++ lib/ep714_food_bill_app/ui/food_bill_home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FoodBillHomePage extends StatefulWidget {
+  @override
+  _FoodBillHomePageState createState() => _FoodBillHomePageState();
+}
+
+class _FoodBillHomePageState extends State<FoodBillHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
