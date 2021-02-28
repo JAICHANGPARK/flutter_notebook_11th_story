7ab15c03ee75bce0b6fa46edea3d5a68dcf2f245
Dreamwalker <aristojeff@gmail.com>
2021-02-28 18:07:23 +0900

    Update food_bill_app.dart

--- lib/ep714_food_bill_app/food_bill_app.dart
+++ lib/ep714_food_bill_app/food_bill_app.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 class FoodBillApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: FoodBillHomePage(),
+    );
   }
 }
