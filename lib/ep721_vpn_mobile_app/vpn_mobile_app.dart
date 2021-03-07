58317fbb3677326aa5e52b6f839162a748938f81
Dreamwalker <aristojeff@gmail.com>
2021-03-07 19:26:08 +0900

    Update vpn_mobile_app.dart

--- lib/ep721_vpn_mobile_app/vpn_mobile_app.dart
+++ lib/ep721_vpn_mobile_app/vpn_mobile_app.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_11th_story/ep721_vpn_mobile_app/ui/vpn_home_page.dart';
 
 class VpnMobileApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: VpnHomePage(),
+    );
   }
 }
