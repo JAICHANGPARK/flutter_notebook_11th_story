fe3a9df35b66bc67fba26ccee206f5c3b4bf65b0
Dreamwalker <aristojeff@gmail.com>
2021-03-10 20:56:34 +0900

    Update bill_split_app.dart

--- lib/ep724_bill_split_app/bill_split_app.dart
+++ lib/ep724_bill_split_app/bill_split_app.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class BillSplitApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: BillSplitHomePage(),
+    );
   }
 }
