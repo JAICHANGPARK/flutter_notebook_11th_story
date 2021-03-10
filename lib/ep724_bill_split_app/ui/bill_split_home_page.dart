a82501f35d0d70b86a06a08a0665940c21aca421
Dreamwalker <aristojeff@gmail.com>
2021-03-10 20:56:56 +0900

    Create bill_split_home_page.dart

---
+++ lib/ep724_bill_split_app/ui/bill_split_home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class BillSplitHomePage extends StatefulWidget {
+  @override
+  _BillSplitHomePageState createState() => _BillSplitHomePageState();
+}
+
+class _BillSplitHomePageState extends State<BillSplitHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
