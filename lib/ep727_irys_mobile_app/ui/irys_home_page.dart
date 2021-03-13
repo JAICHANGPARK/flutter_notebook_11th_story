a9e3d7e7857463f553fc4b9bf5777485e02dbb55
Dreamwalker <aristojeff@gmail.com>
2021-03-13 20:03:55 +0900

    Update irys_home_page.dart

--- lib/ep727_irys_mobile_app/ui/irys_home_page.dart
+++ lib/ep727_irys_mobile_app/ui/irys_home_page.dart
@@ -12,44 +12,44 @@ class _IrysHomePageState extends State<IrysHomePage> {
       body: SafeArea(
         child: Stack(
           children: [
-            Positioned(child: Column(children: [
-              
-            ],)),
-            Positioned(child: Card(
-              child: Row(
-                children: [
-                  Row(
+            Positioned(
+                child: Column(
+              children: [],
+            )),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 16,
+              child: SizedBox(
+                height: 32,
+                width: 160,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue
+                  ),
+                  child: Row(
                     children: [
-                      Icon(Icons.list),
-                      Text("List View")
+                      Row(
+                        children: [Icon(Icons.list), Text("List View")],
+                      ),
+                      Row(
+                        children: [Icon(Icons.list), Text("List View")],
+                      ),
                     ],
                   ),
-                  Row(
-                    children: [
-                      Icon(Icons.list),
-                      Text("List View")
-                    ],
-                  ),
-                ],
+                ),
               ),
-            ), left: 0, right: 0, bottom: 16,),
+
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {  },),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.greenAccent,
+        child: Icon(Icons.chat),
+        foregroundColor: Colors.black,
+      ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
