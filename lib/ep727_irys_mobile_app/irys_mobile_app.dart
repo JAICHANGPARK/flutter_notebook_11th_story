92337ba6c1faeab17e7e17bdc9be471e06b194b9
Dreamwalker <aristojeff@gmail.com>
2021-03-13 19:56:17 +0900

    Update irys_mobile_app.dart

--- lib/ep727_irys_mobile_app/irys_mobile_app.dart
+++ lib/ep727_irys_mobile_app/irys_mobile_app.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class IrysMobileApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+
+    );
   }
 }
