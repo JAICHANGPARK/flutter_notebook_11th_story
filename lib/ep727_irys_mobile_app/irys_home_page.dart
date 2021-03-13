0a8ed1bf50809e752544220f78236ced29d0aa0d
Dreamwalker <aristojeff@gmail.com>
2021-03-13 19:56:36 +0900

    Update irys_home_page.dart

--- lib/ep727_irys_mobile_app/irys_home_page.dart
+++ lib/ep727_irys_mobile_app/irys_home_page.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class IrysHomePage extends StatefulWidget {
+  @override
+  _IrysHomePageState createState() => _IrysHomePageState();
+}
+
+class _IrysHomePageState extends State<IrysHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
