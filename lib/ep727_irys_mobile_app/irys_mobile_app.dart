29e10c733b397241e73fa79d789001e88f1d0871
Dreamwalker <aristojeff@gmail.com>
2021-03-13 19:56:53 +0900

    Update irys_mobile_app.dart

--- lib/ep727_irys_mobile_app/irys_mobile_app.dart
+++ lib/ep727_irys_mobile_app/irys_mobile_app.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'irys_home_page.dart';
+
 class IrysMobileApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+        home: IrysHomePage(),
     );
   }
 }
