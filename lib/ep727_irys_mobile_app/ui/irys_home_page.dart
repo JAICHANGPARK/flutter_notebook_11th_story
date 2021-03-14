717167c02f1df0b32366bf0755774cb531c4e7d9
Dreamwalker <aristojeff@gmail.com>
2021-03-14 18:34:38 +0900

    Update irys_home_page.dart

--- lib/ep727_irys_mobile_app/ui/irys_home_page.dart
+++ lib/ep727_irys_mobile_app/ui/irys_home_page.dart
@@ -232,7 +232,12 @@ class _IrysHomePageState extends State<IrysHomePage> with SingleTickerProviderSt
                                             ),
                                             Expanded(
                                               child: Column(
-                                                children: [],
+                                                children: [
+                                                  Row(
+                                                    children: [Text("FLOODING"), Spacer(), Text('2 July 2020')],
+                                                  ),
+                                                  Text("3517 W. Gray St. Tulca, Pennsylvania 57867")
+                                                ],
                                               ),
                                               flex: 2,
                                             ),
