e6a73bba840570f1b7d9e9ce20e36a75dc6865a5
Dreamwalker <aristojeff@gmail.com>
2021-03-15 19:45:58 +0900

    Update irys_profile_page.dart

--- lib/ep727_irys_mobile_app/ui/irys_profile_page.dart
+++ lib/ep727_irys_mobile_app/ui/irys_profile_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class IrysProfilePage extends StatefulWidget {
   @override
   _IrysProfilePageState createState() => _IrysProfilePageState();
@@ -10,10 +9,17 @@ class _IrysProfilePageState extends State<IrysProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Column(children: [
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            IconButton(onPressed: (){
+              Navigator.of(context).pop();
 
-        
-      ],),),
+            }, icon: Icon(Icons.arrow_back_ios)),
+          ],
+        ),
+      ),
     );
   }
 }
