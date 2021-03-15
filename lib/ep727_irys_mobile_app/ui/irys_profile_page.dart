6341e4c3b2cb7478e9b7a10ba6c0fd5689639b26
Dreamwalker <aristojeff@gmail.com>
2021-03-15 19:43:16 +0900

    Update irys_profile_page.dart

---
+++ lib/ep727_irys_mobile_app/ui/irys_profile_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class IrysProfilePage extends StatefulWidget {
+  @override
+  _IrysProfilePageState createState() => _IrysProfilePageState();
+}
+
+class _IrysProfilePageState extends State<IrysProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
