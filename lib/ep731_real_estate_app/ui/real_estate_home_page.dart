7e33278dbc3438baa9b05d0b7c4041b5cc7d853d
Dreamwalker <aristojeff@gmail.com>
2021-03-17 19:45:21 +0900

    Create real_estate_home_page.dart

---
+++ lib/ep731_real_estate_app/ui/real_estate_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RealEstateHomePage extends StatefulWidget {
+  RealEstateHomePage({Key key}) : super(key: key);
+
+  @override
+  _RealEstateHomePageState createState() => _RealEstateHomePageState();
+}
+
+class _RealEstateHomePageState extends State<RealEstateHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
