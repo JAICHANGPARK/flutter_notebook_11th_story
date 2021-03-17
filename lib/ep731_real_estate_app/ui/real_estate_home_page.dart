0c0966fce4826d7355ad6514b44d0d352f5f52e8
Dreamwalker <aristojeff@gmail.com>
2021-03-17 19:50:40 +0900

    Update real_estate_home_page.dart

--- lib/ep731_real_estate_app/ui/real_estate_home_page.dart
+++ lib/ep731_real_estate_app/ui/real_estate_home_page.dart
@@ -15,6 +15,21 @@ class _RealEstateHomePageState extends State<RealEstateHomePage> {
         child: Stack(
           children: [
             Positioned(child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.white,
+                    Colors.grey[300]!,
+                  ],
+                  begin: Alignment.topCenter
+                )
+              ),
+            ),
+            left: 0,
+            bottom: 0,
+            top: 0,
+            right: 0,),
+            Positioned(child: Container(
               height: 64,
               decoration: BoxDecoration(
                 color: Colors.white
