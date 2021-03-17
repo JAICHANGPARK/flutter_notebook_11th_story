2f341a5ee3d65f660e5b0583923654dc701e970c
Dreamwalker <aristojeff@gmail.com>
2021-03-17 19:52:15 +0900

    Update real_estate_home_page.dart

--- lib/ep731_real_estate_app/ui/real_estate_home_page.dart
+++ lib/ep731_real_estate_app/ui/real_estate_home_page.dart
@@ -14,38 +14,39 @@ class _RealEstateHomePageState extends State<RealEstateHomePage> {
       body: SafeArea(
         child: Stack(
           children: [
-            Positioned(child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Colors.white,
-                    Colors.grey[50]!,
-                    Colors.grey[100]!,
-                    Colors.grey[200]!,
-                    Colors.grey[300]!,
-                  ],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter
-                )
-              ),
-            ),
-            left: 0,
-            bottom: 0,
-            top: 0,
-            right: 0,),
             Positioned(
+              child: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                  Colors.white,
+                  Colors.grey[50]!,
+                  Colors.grey[100]!,
+                  Colors.grey[200]!,
+                  Colors.grey[300]!,
+                ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+              ),
+              left: 0,
               bottom: 0,
+              top: 0,
+              right: 0,
+            ),
+            Positioned(
+                bottom: 0,
                 left: 0,
                 right: 0,
                 child: PhysicalModel(
                   color: Colors.black.withOpacity(0.4),
                   elevation: 2,
                   child: Container(
-              height: 64,
-              decoration: BoxDecoration(
-                  color: Colors.white
-              ),
-            ),
+                    height: 64,
+                    decoration: BoxDecoration(color: Colors.white),
+                    child: Row(
+                      children: [
+                        
+                      ],
+                    ),
+
+                  ),
                 )),
           ],
         ),
