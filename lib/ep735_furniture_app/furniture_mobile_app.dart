751517399ccf1dcb7fc16bbbd3adfe20f1eb8e2e
Dreamwalker <aristojeff@gmail.com>
2021-03-21 18:03:54 +0900

    Update furniture_mobile_app.dart

--- lib/ep735_furniture_app/furniture_mobile_app.dart
+++ lib/ep735_furniture_app/furniture_mobile_app.dart
@@ -7,6 +7,7 @@ class FurnitureMobileApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: "/",
       routes: {
         "/" : (context)=>FurnitureHomePage()
       },
