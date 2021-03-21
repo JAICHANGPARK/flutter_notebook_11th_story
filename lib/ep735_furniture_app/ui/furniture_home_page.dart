b39379cf753f2230aebb52fca80702f402858c4e
Dreamwalker <aristojeff@gmail.com>
2021-03-21 17:50:22 +0900

    Create furniture_home_page.dart

---
+++ lib/ep735_furniture_app/ui/furniture_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FurnitureHomePage extends StatefulWidget {
+  FurnitureHomePage({Key key}) : super(key: key);
+
+  @override
+  _FurnitureHomePageState createState() => _FurnitureHomePageState();
+}
+
+class _FurnitureHomePageState extends State<FurnitureHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
