4a751e4c743b947193de2410758f363ac3a51aef
Dreamwalker <aristojeff@gmail.com>
2021-03-21 17:50:58 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'ep731_real_estate_app/real_estate_app.dart';
+import 'ep735_furniture_app/furniture_mobile_app.dart';
 
 void main() {
-  runApp(RealEstateApp());
+  runApp(FurnitureMobileApp());
 }
