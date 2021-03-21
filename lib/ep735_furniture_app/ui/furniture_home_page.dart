767165b79baf5a09ea8ced1461ccbd3118eceded
Dreamwalker <aristojeff@gmail.com>
2021-03-21 18:03:18 +0900

    Update furniture_home_page.dart

--- lib/ep735_furniture_app/ui/furniture_home_page.dart
+++ lib/ep735_furniture_app/ui/furniture_home_page.dart
@@ -12,70 +12,72 @@ class _FurnitureHomePageState extends State<FurnitureHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey[50],
-      body: Stack(
-        children: [
-          Positioned(
-            top: 0,
-            right: 0,
-            left: 0,
-            bottom: 96,
-            child: Column(
-              children: [
-                Expanded(
-                    flex:2,
-                    child: Placeholder()),
-                Expanded(
-                    flex:2,
-                    child: Placeholder()),
-                Expanded(
-                    flex:1,
-                    child: Placeholder()),
-                Expanded(
-                    flex:8,
-                    child: Placeholder()),
-              ],
-            ),
-          ),
-          Positioned(
-              bottom: 0,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              top: 0,
               right: 0,
               left: 0,
-              child: Container(
-                  height: 72,
-                  decoration: BoxDecoration(
-                      color: Colors.indigo,
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(16), topRight: Radius.circular(16))),
-                  padding: EdgeInsets.symmetric(horizontal: 36),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      IconButton(
-                        onPressed: () {},
-                        iconSize: 32,
-                        icon: Icon(Icons.home_filled),
-                        color: Colors.white,
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        iconSize: 32,
-                        icon: Icon(Icons.shopping_bag_outlined),
-                        color: Colors.white,
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        iconSize: 32,
-                        icon: Icon(Icons.star_border),
-                        color: Colors.white,
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        iconSize: 32,
-                        icon: Icon(Icons.person_outline),
-                        color: Colors.white,
-                      )
-                    ],
-                  )))
-        ],
+              bottom: 96,
+              child: Column(
+                children: [
+                  Expanded(
+                      flex:2,
+                      child: Placeholder()),
+                  Expanded(
+                      flex:2,
+                      child: Placeholder()),
+                  Expanded(
+                      flex:1,
+                      child: Placeholder()),
+                  Expanded(
+                      flex:10,
+                      child: Placeholder()),
+                ],
+              ),
+            ),
+            Positioned(
+                bottom: 0,
+                right: 0,
+                left: 0,
+                child: Container(
+                    height: 72,
+                    decoration: BoxDecoration(
+                        color: Colors.indigo,
+                        borderRadius: BorderRadius.only(topLeft: Radius.circular(16), topRight: Radius.circular(16))),
+                    padding: EdgeInsets.symmetric(horizontal: 36),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          iconSize: 32,
+                          icon: Icon(Icons.home_filled),
+                          color: Colors.white,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          iconSize: 32,
+                          icon: Icon(Icons.shopping_bag_outlined),
+                          color: Colors.white,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          iconSize: 32,
+                          icon: Icon(Icons.star_border),
+                          color: Colors.white,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          iconSize: 32,
+                          icon: Icon(Icons.person_outline),
+                          color: Colors.white,
+                        )
+                      ],
+                    )))
+          ],
+        ),
       ),
     );
   }
