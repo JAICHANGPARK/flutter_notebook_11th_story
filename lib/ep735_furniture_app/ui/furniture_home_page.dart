b0f13c134ce5607020ed4ee32029fb56d338e2fe
Dreamwalker <aristojeff@gmail.com>
2021-03-22 20:07:31 +0900

    Update furniture_home_page.dart

--- lib/ep735_furniture_app/ui/furniture_home_page.dart
+++ lib/ep735_furniture_app/ui/furniture_home_page.dart
@@ -23,18 +23,32 @@ class _FurnitureHomePageState extends State<FurnitureHomePage> {
               bottom: 96,
               child: Column(
                 children: [
-                  Expanded(flex: 2, child: Row(
-                    children: [
-                      CircleAvatar(),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                  Expanded(
+                      flex: 2,
+                      child: Row(
                         children: [
-                          Text("Welcome,"),
-                          Text("Courtney Henry"),
+                          CircleAvatar(),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Welcome,"),
+                              Text("Courtney Henry"),
+                            ],
+                          ),
+                          Container(
+                            height: 32,
+                            width: 32,
+                            child: Stack(
+                              children: [
+                                Positioned(
+                                    child: IconButton(
+                                  icon: Icon(Icons.notifications_none), onPressed: () {  },
+                                ))
+                              ],
+                            ),
+                          )
                         ],
-                      )
-                    ],
-                  )),
+                      )),
                   Expanded(flex: 2, child: Placeholder()),
                   Expanded(flex: 1, child: Placeholder()),
                   Expanded(flex: 10, child: Placeholder()),
