b66c3e9c58329b58c683a7a212e55ad5876ea6ed
Dreamwalker <aristojeff@gmail.com>
2021-03-23 23:56:41 +0900

    Update furniture_home_page.dart

--- lib/ep735_furniture_app/ui/furniture_home_page.dart
+++ lib/ep735_furniture_app/ui/furniture_home_page.dart
@@ -151,6 +151,26 @@ class _FurnitureHomePageState extends State<FurnitureHomePage> {
                               Expanded(child: Placeholder()),
                             ],
                           ),
+                        ),
+                        Card(
+                          child: Column(
+                            children: [
+                              Expanded(child: Placeholder()),
+                              Expanded(child: Placeholder()),
+                              Expanded(child: Placeholder()),
+                              Expanded(child: Placeholder()),
+                            ],
+                          ),
+                        ),
+                        Card(
+                          child: Column(
+                            children: [
+                              Expanded(child: Placeholder()),
+                              Expanded(child: Placeholder()),
+                              Expanded(child: Placeholder()),
+                              Expanded(child: Placeholder()),
+                            ],
+                          ),
                         )
 
                       ],
