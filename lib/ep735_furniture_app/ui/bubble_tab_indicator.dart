0316f578ccf80a14afdb416579cf2ca8e6965a43
Dreamwalker <aristojeff@gmail.com>
2021-03-25 19:26:56 +0900

    Update bubble_tab_indicator.dart

--- lib/ep735_furniture_app/ui/bubble_tab_indicator.dart
+++ lib/ep735_furniture_app/ui/bubble_tab_indicator.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
 class BubbleTabIndicator extends Decoration{
+  final double indicatorHeight;
+  final Color indicatorColor;
+  final double indicatorRadius;
+  final EdgeInsetsGeometry padding;
+  final EdgeInsetsGeometry insets;
+  final EdgeInsetsGeometry padding;
+
+  BubbleTabIndicator(this.indicatorHeight, this.indicatorColor);
+
+
   @override
   BoxPainter createBoxPainter([VoidCallback onChanged]) {
     // TODO: implement createBoxPainter
     throw UnimplementedError();
   }
-  
+
 }
