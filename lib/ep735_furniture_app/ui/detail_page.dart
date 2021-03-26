315b651309e4afef6d50230f378b5d329b87fd5e
Dreamwalker <aristojeff@gmail.com>
2021-03-27 00:09:45 +0900

    Update detail_page.dart

--- lib/ep735_furniture_app/ui/detail_page.dart
+++ lib/ep735_furniture_app/ui/detail_page.dart
@@ -89,7 +89,9 @@ class _FurnitureDetailPageState extends State<FurnitureDetailPage> {
                   Text("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
                   style: TextStyle(
                     fontSize: 12,
-                    
+                    color: Colors.grey,
+                    wordSpacing: 1.2,
+                    letterSpacing: 1.2
                   ),
                   ),
                   SizedBox(height: 24),
