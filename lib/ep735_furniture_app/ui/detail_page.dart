e48c7858be96793b4c513baef127836f43c9351e
Dreamwalker <aristojeff@gmail.com>
2021-03-26 23:42:05 +0900

    Create detail_page.dart

---
+++ lib/ep735_furniture_app/ui/detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FurnitureDetailPage extends StatefulWidget {
+  @override
+  _FurnitureDetailPageState createState() => _FurnitureDetailPageState();
+}
+
+class _FurnitureDetailPageState extends State<FurnitureDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
