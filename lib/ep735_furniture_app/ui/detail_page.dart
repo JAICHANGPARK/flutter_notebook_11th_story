db04f61c94406e1a98d8ba390394c850af5f8bcf
Dreamwalker <aristojeff@gmail.com>
2021-03-26 23:49:28 +0900

    Update detail_page.dart

--- lib/ep735_furniture_app/ui/detail_page.dart
+++ lib/ep735_furniture_app/ui/detail_page.dart
@@ -12,20 +12,20 @@ class _FurnitureDetailPageState extends State<FurnitureDetailPage> {
       body: Stack(
         children: [
           Positioned(
-            left: 0,
+              left: 0,
               right: 0,
               top: 0,
               child: Container(
-            height: MediaQuery.of(context).size.height / 4,
-          )),
+                height: MediaQuery.of(context).size.height / 4,
+                color: Colors.grey[200],
+              )),
           Positioned(
             child: Container(
               height: MediaQuery.of(context).size.height / 3,
               child: Column(
                 children: [
-                  
-                ],
 
+                ],
               ),
             ),
             left: 0,
