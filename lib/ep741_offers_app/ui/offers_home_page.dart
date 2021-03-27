04968b59312845dbb64ad3dc6ceced7126af7c93
Dreamwalker <aristojeff@gmail.com>
2021-03-27 18:09:47 +0900

    Update offers_home_page.dart

---
+++ lib/ep741_offers_app/ui/offers_home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OffersHomePage extends StatefulWidget {
+  @override
+  _OffersHomePageState createState() => _OffersHomePageState();
+}
+
+class _OffersHomePageState extends State<OffersHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        
+      ),
+    );
+  }
+}
