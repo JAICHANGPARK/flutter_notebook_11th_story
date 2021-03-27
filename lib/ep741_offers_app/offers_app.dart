487f96f3e93ac871d2e40ca06936cde41e59be5b
Dreamwalker <aristojeff@gmail.com>
2021-03-27 18:10:52 +0900

    Update offers_app.dart

--- lib/ep741_offers_app/offers_app.dart
+++ lib/ep741_offers_app/offers_app.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_11th_story/ep741_offers_app/ui/offers_home_page.dart';
 
 class OffersApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: OffersHomePage(),
     );
   }
 }
