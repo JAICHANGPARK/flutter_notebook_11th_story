e022aa362f113ab5e1e57428af5ec6a6599015b1
Dreamwalker <aristojeff@gmail.com>
2021-03-28 22:30:33 +0900

    Update offers_home_page.dart

--- lib/ep741_offers_app/ui/offers_home_page.dart
+++ lib/ep741_offers_app/ui/offers_home_page.dart
@@ -66,13 +66,13 @@ class _OffersHomePageState extends State<OffersHomePage> {
                               children: [
                                 Expanded(
                                     child: Container(
-                                      decoration: BoxDecoration(
-                                     image: DecorationImage(
-                                       image: NetworkImage(
-                                         "https://cdn.pixabay.com/photo/2015/10/20/18/57/furniture-998265_960_720.jpg",
-                                       )
-                                     )
-                                      ),
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                    image: NetworkImage(
+                                      "https://cdn.pixabay.com/photo/2015/10/20/18/57/furniture-998265_960_720.jpg",
+                                    ),
+                                    fit: BoxFit.cover,
+                                  )),
                                   child: Stack(
                                     children: [],
                                   ),
