80ada0cf0af54cbd5992f47db4bc962c4ed425db
Dreamwalker <aristojeff@gmail.com>
2021-03-30 19:48:53 +0900

    Update food_app.dart

--- lib/ep744_food_app/food_app.dart
+++ lib/ep744_food_app/food_app.dart
@@ -1,1 +1,11 @@
 import 'package:flutter/material.dart';
+
+class FoodApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FoodHomePage(),
+    );
+  }
+}
+
