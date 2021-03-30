08e54b97e9c00646fe55f298d9bb2d9d70312c35
Dreamwalker <aristojeff@gmail.com>
2021-03-30 19:50:08 +0900

    Create food_home_page.dart

---
+++ lib/ep744_food_app/ui/food_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FoodHomePage extends StatefulWidget {
+  FoodHomePage({Key key}) : super(key: key);
+
+  @override
+  _FoodHomePageState createState() => _FoodHomePageState();
+}
+
+class _FoodHomePageState extends State<FoodHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
