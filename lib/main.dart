24aaa28b6dbeac7a734e741e3b3d23a07b288335
Dreamwalker <aristojeff@gmail.com>
2021-03-30 19:49:34 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'ep741_offers_app/offers_app.dart';
+import 'ep744_food_app/food_app.dart';
 
 void main() {
-  runApp(OffersApp());
+  runApp(FoodApp());
 }
