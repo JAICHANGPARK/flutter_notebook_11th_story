ae316a2dcdc47d62a21f5bddf43a2b9e19942c44
Dreamwalker <aristojeff@gmail.com>
2021-04-01 20:06:09 +0900

    Update food_detail_page.dart

--- lib/ep744_food_app/ui/food_detail_page.dart
+++ lib/ep744_food_app/ui/food_detail_page.dart
@@ -1,2 +1,13 @@
 import 'package:flutter/material.dart';
 
+class FoodDetailPage extends StatefulWidget {
+  @override
+  _FoodDetailPageState createState() => _FoodDetailPageState();
+}
+
+class _FoodDetailPageState extends State<FoodDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
