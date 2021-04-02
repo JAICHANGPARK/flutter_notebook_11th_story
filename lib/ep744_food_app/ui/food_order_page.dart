a551b1b079413d38ea1ad191f142c57e2f7b61a2
Dreamwalker <aristojeff@gmail.com>
2021-04-02 20:29:01 +0900

    Update food_order_page.dart

---
+++ lib/ep744_food_app/ui/food_order_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FoodOrderPage extends StatefulWidget {
+  @override
+  _FoodOrderPageState createState() => _FoodOrderPageState();
+}
+
+class _FoodOrderPageState extends State<FoodOrderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
