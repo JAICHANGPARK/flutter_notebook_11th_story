c6a82b5543b4c90787fbce8b84ccec16620da832
Dreamwalker <aristojeff@gmail.com>
2021-04-02 20:40:12 +0900

    Update food_order_page.dart

--- lib/ep744_food_app/ui/food_order_page.dart
+++ lib/ep744_food_app/ui/food_order_page.dart
@@ -95,7 +95,10 @@ class _FoodOrderPageState extends State<FoodOrderPage> {
             Expanded(flex: 2, child: Container(
               margin: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
               decoration: BoxDecoration(
-                color: Colors.black
+                color: Colors.black,
+              ),
+              child: Center(
+                child: Text("Place order"),
               ),
             )),
           ],
