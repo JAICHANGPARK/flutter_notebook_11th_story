3ec38ddefa1b586ebed2696a1052bc1c0f6201d7
Dreamwalker <aristojeff@gmail.com>
2021-04-04 18:47:17 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'ep744_food_app/food_app.dart';
+import 'ep749_bank_app/ep749_bank_app.dart';
 
 void main() {
-  runApp(FoodApp());
+  runApp(Ep749BankApp());
 }
