90c2b2d028d5e62a9f8624cfd633c065467d16a9
Dreamwalker <aristojeff@gmail.com>
2021-04-04 18:49:37 +0900

    Create bank_balance_page.dart

---
+++ lib/ep749_bank_app/ui/bank_balance_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BankBalancePage extends StatefulWidget {
+  @override
+  _BankBalancePageState createState() => _BankBalancePageState();
+}
+
+class _BankBalancePageState extends State<BankBalancePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
