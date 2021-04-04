42a06352f89a0f755e9589773f3245fcd4a94929
Dreamwalker <aristojeff@gmail.com>
2021-04-04 18:46:05 +0900

    Update ep749_bank_app.dart

---
+++ lib/ep749_bank_app/ep749_bank_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class Ep749BankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BankBalancePage(),
+    );
+  }
+}
